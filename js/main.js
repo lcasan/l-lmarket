a2e04998351d06872da97206b41cc6d3c36de35b
lcasan <lcasan120100@gmail.com>
2024-10-06 15:50:15 -0400

    Show products index.html

---
+++ js/main.js
@@ -0,0 +1,21 @@
+import { Product } from "./models.js";
+import stock from "../data/stock.json" assert {type: 'json'}
+
+const app = document.querySelector('.products__container');
+
+if (app) {
+    stock['pullovers'].map(item => {
+        const product = new Product({
+            code: item['code'],
+            name: item['name'],
+            color: item['color'],
+            size: item['size'],
+            price: item['price']
+        });
+
+        const productElement = product.render();
+        app.appendChild(productElement);
+    });
+} else {
+    console.error("El contenedor '.products__container' no existe en el DOM.");
+}
